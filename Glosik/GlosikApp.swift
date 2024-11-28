8e52298d8d5b6db59352b71559c4f1ecdcfd5632
Rudrank Riyam <rudrankriyam@gmail.com>
2024-11-29 01:33:42 +0530

    Initial commit

---
+++ Glosik/GlosikApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlosikApp.swift
+//  Glosik
+//
+//  Created by Rudrank Riyam on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GlosikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
